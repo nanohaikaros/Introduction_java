1b237df5b3ab7dd77c1b202b5d18f1eeb61cc988
Nanoha <1371689491@qq.com>
2019-08-14 18:09:23 +0800

    1

---
+++ src/RandomCharacter.java
@@ -0,0 +1,21 @@
+public class RandomCharacter {
+    public static char getRandomCharacter(char ch1, char ch2) {
+        return (char)(ch1 + Math.random() * (ch1 - ch2 + 1));
+    }
+
+    public static char getRandomLowerCaseLetter() {
+        return getRandomCharacter('a', 'z');
+    }
+
+    public static char getRandomUpperCaseLetter() {
+        return getRandomCharacter('A', 'Z');
+    }
+
+    public static char getRandomDigitCharacter() {
+        return getRandomCharacter('0', '9');
+    }
+
+    public static char getRandomCharacter() {
+        return getRandomCharacter('\u0000', '\uFFFF');
+    }
+}
